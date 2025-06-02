2f764b356f07f10beff5f82654611745ba2fcaad
feliciocavalcante <feliciospfc@gmail.com>
2025-06-02 18:19:02 -0300

    fixed styled import

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import styles from './index.module.css';
+import { useState } from 'react';
+import * as styles from './index.module.css';
 import tenis from '../../assets/img/tenis.png';
 import { Link } from 'react-router-dom';
 import { supabase } from '../SupabaseClient/supabaseClient';
 import { useNavigate } from 'react-router-dom';
-
-
 
 export default function Index() {
   const [email, setEmail] = useState('');
